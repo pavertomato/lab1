d818fc1f83b63de47659474a4e69937cf1c2000f
Evgenii Lezhnin <z_lezhnin@mail2000.ru>
2013-09-10 17:34:49 +0700

    input

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 main :
 	g++ -o lab1 main.cpp
+main-debug :
+	g++ -g -O0 -o lab1 main.cpp
--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
 int main(int argc, char **argv) {
-	return 0;
+    int n;
+    string spol;
+    double a,b,eps;
+    cin >> n;
+    getline(cin,spol);
+    getline(cin,spol);
+    cin >> a,b;
+    cin >> eps;
+
+    return 0;
 }
